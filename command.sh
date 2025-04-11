658efd6975ad5a32e8e5cddafb2ed40bbaa4984b
aishuaiyao <aishuaiyao@163.com>
2025-04-11 15:06:33 +0800

    #

---
+++ command.sh
@@ -0,0 +1,24 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple chromadb==0.6.3 pypdf==5.3.0 unstructured[pdf]==0.16.23
+
+
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple cu124
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+pip install torch==2.6.0+cu124 -i https://pypi.tuna.tsinghua.edu.cn/simple --extra-index-url https://download.pytorch.org/whl/cu124
+
+
+
+
+
