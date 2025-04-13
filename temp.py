5f027f9b3730b7a792c86f3da7c0b489cbf458ee
yaoaishuai <aishuaiyao@163.com>
2025-04-13 17:55:14 +0800

    update

--- temp.py
+++ temp.py
@@ -1,37 +1,30 @@
 import uuid
 import json
 
-template = {
-    'requestid': 'x',
-    'path1': {'traceid': 'x', 'length': 53, 'turnleft': 4, 'turnright': 4, "safescore": 4.3},
-    'path2': {'traceid': 'x', 'length': 52, 'turnleft': 4, 'turnright': 4, "safescore": 4.5},
-    'best': 'path2',
-    'reason': "shorter and safe than path1"
+with open('dataset/case.json', 'r', encoding='utf-8') as fr:
+    cases = json.load(fr)
 
-}
-
-for i in range(1):
+cases_txt = []
+for template in cases:
     template['requestid'] = str(uuid.uuid4()).replace('-', '')
     template['path1']['traceid'] = str(uuid.uuid4()).replace('-', '')
     template['path2']['traceid'] = str(uuid.uuid4()).replace('-', '')
-    # print(json.dumps(template, ensure_ascii=False, indent=4))
     print(json.dumps(template))
+    cases_txt.append(template)
 
+with open('dataset/cases.txt', 'r') as fw:
+    fw.writelines(cases_txt)
 
 data = {
     "requestid": "306a7b8e17b011f0b173e02be94eee68",
     "path1": {
         "traceid": "306a7b8f17b011f0b5d9e02be943ee63",
-        "length": 589,
-        "turnleft": 4,
-        "turnright": 4,
+        "length": 1783.59,
         "safescore": 4.3
     },
     "path2": {
         "traceid": "306a7b9017b011f0af97e02be943ee62",
-        "length": 549,
-        "turnleft": 4,
-        "turnright": 4,
+        "length": 1549.53,
         "safescore": 4.9
     }
 }
